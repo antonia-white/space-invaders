0025faf66e3eb0863a700aa7a64d3333ad1dbcad
Toni <antonia.c.white1@gmail.com>
2022-03-29 19:45:36 +0000

    Add basic logic plan to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+/* jshint esversion: 8, jquery: true */
+
+// set initial variables
+
+// event listeners for game input
+    // start game
+    // keydown move ship
+    // keydown shoot
+
+function startGame() {
+
+};
+
+function endGame() {
+
+};
+
+function resetGame() {
+
+};
+
+function moveShipLeft() {
+
+};
+
+function moveShipRight() {
+
+};
+
+function shoot() {
+
+};
+
+function generateAlienInvasion() {
+
+};
