05b427ef2a392470aa6e42a39b6cc5c697b1a2fc
Toni <antonia.c.white1@gmail.com>
2022-04-04 21:54:10 +0000

    Add alien attributes to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,24 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+alienInfo = {
+    //height and width of each alien
+    width: 40,
+    height: 20,
+    //how many rows and collumns of aliens
+    count: {
+        row: 4,
+        col: 9
+    },
+    //offset aliens from canvas
+    offset: {
+        top: 100,
+        left: 60
+    },
+    //padding between aliens
+    padding: 5
+}
+
 function preload() {
 
 };
