c83f533cbc4bb7af969180c73da2f3f3f1e4294b
Toni <antonia.c.white1@gmail.com>
2022-04-18 14:15:09 +0000

    Add functionality to have start button close modal in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -371,7 +371,7 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let startBtn = document.getElementById("startBtn");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -379,13 +379,6 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+startBtn.onclick = function() {
   modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
