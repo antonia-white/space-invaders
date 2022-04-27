497fa2f5a4d31cfc60e53565495a139310128939
Toni <antonia.c.white1@gmail.com>
2022-04-27 10:08:17 +0000

    Add test for setting audio image functionality

--- assets/js/tests/modals.test.js
+++ assets/js/tests/modals.test.js
@@ -2,44 +2,53 @@
  * @jest-environment jsdom
  */
 
-let setAudioImage;
+ let setAudioImage;
 
-beforeAll(() => {
-    let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html", "utf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
-
-    setAudioImage = require('../modals.js')
-})
-
-// Modal navigation
-describe("modals open and buttons navigate to the correct modal", () => {
-    test("modals exist", () =>{
-        expect("homeModal" in document).toBeDefined();
-        expect("instructionsModal" in document).toBeDefined();
-        expect("scoreboardModal" in document).toBeDefined();
-        expect("endGameModal" in document).toBeDefined();
-    });
-    test("instructios modal shows when instruction button is clicked", () => {
-        
-    });
-});
-
-// Audio toggle
-describe("audio checkbox toggles, returning a boolean value as expected", () => {
-    test("button exists", () => {
-        expect("audio-check" in document).toBeDefined();
-    });
-    test("audio checkbox is not ticked", () => {
-        expect("audio-check" in document).toBeFalsy();
-    });
-})
-
-// // Scoreboard
-// describe("scoreboard generates a list of maximum ten elements", () => {
-//     test("scoreboard exists", () => {
-//         expect("scoreboard-list" in document).toBeDefined();
-//     });
-// })
+ // const { setAudioImage } = require("../modals.js");
+ 
+ beforeAll(() => {
+     let fs = require("fs");
+     let fileContents = fs.readFileSync("index.html", "utf-8");
+     document.open();
+     document.write(fileContents);
+     document.close();
+ 
+     setAudioImage = require('../modals.js')
+ })
+ 
+ // Modal navigation
+ describe("modals open and buttons navigate to the correct modal", () => {
+     test("modals exist", () => {
+         expect("homeModal" in document).toBeDefined();
+         expect("instructionsModal" in document).toBeDefined();
+         expect("scoreboardModal" in document).toBeDefined();
+         expect("endGameModal" in document).toBeDefined();
+     });
+     test("instructios modal shows when instruction button is clicked", () => {
+ 
+     });
+ });
+ 
+ // Audio toggle
+ describe("audio checkbox toggles, returning a boolean value as expected", () => {
+     test("button exists", () => {
+         expect("audio-check" in document).toBeDefined();
+     });
+     test("audio checkbox is not ticked", () => {
+         expect("audio-check" in document).toBeFalsy();
+     });
+ })
+ 
+ // Audio image
+ describe("audio image displays correctly", () => {
+     beforeAll(() => {
+         let checkbox = document.getElementById("audio-check");
+         checkbox.checked = true;
+         setAudioImage();
+     });
+     test("expect setAudioImage function works correctly", () => {
+         expect((document.getElementById("audio-off-img").style.display).toBe("none"));
+         expect((document.getElementById("audio-on-img").style.display).toBe("block"));
+     });
+ });
+ 
